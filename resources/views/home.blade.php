a9a07c215c771d07b2b8a8b3fa13d220cf9b37ec
mateo <mateobucarey017@gmail.com>
2025-06-09 12:20:58 -0300

    detalles esteticos home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-center">
-        <h1 class="text-4xl font-bold mb-4">Bienvenido al blog de comidas tradicionales argentinas</h1>
-        <p class="text-lg">Descubre recetas auténticas, comparte tus propias creaciones y conectate con la tradición culinaria.</p>
+    <div 
+        class="bg-cover bg-center flex items-center justify-center text-center text-white py-20"
+        style="background-image: url('/images/fondo2.jpg');"
+    >
+        <div class="bg-black bg-opacity-60 p-10 rounded-lg max-w-3xl">
+            <h1 class="text-4xl font-bold mb-4">
+                Bienvenido al blog de comidas tradicionales argentinas
+            </h1>
+            <p class="text-lg">
+                Descubre recetas auténticas, comparte tus propias creaciones y conectate con la tradición culinaria.
+            </p>
+        </div>
     </div>
 @endsection
